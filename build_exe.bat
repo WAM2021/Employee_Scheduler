9b558a6a1a9e26889b0e1219bf0ce1b7abfc36b5
wmkm9 <wmkm9@mail.umsl.edu>
2025-10-27 12:55:28 -0500

    Update build script for v1.0.4

--- build_exe.bat
+++ build_exe.bat
@@ -1,12 +1,12 @@
 @echo off
-echo Building Employee Scheduler v1.0.3 executable...
+echo Building Employee Scheduler v1.0.4 executable...
 echo.
 
 REM Install PyInstaller if not already installed
 pip install pyinstaller
 
 REM Build the executable using the spec file
-pyinstaller Employee_Scheduler_v1.0.3.spec
+pyinstaller Employee_Scheduler_v1.0.4.spec
 
 echo.
 echo Build complete! 
